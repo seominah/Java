cbd880bb976658d7d97a8ab9bfc8c80b98d89601
seominah <seominah95@gmail.com>
2021-12-20 22:07:53 +0900

    구구단 2단, 3단

---
+++ src/inflearn/Gugudan.java
@@ -0,0 +1,18 @@
+package inflearn;
+
+public class Gugudan {
+    public static void main(String[] args) {
+        //2단
+        // 0 ~ 9까지 반복한다.
+        for(int i = 1; i < 10; i++) {
+            // 2의 배수 10개의 값을 출력한다.
+            System.out.println("2 * " + i + " = " + 2 * i);
+        }
+        //3단
+        // 0 ~ 9까지 반복한다.
+        for(int i = 1; i < 10; i++) {
+            // 3의 배수 10개의 값을 출력한다.
+            System.out.println("3 * " + i + " = " + 3 * i);
+        }
+    }
+}
