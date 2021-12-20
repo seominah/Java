732e58d86952013f272c8531406c4a6d09d210e4
seominah <seominah95@gmail.com>
2021-12-20 23:25:17 +0900

    배열로 구구단 2-9단 구현

---
+++ src/inflearn/ArrayGugudan.java
@@ -0,0 +1,31 @@
+package inflearn;
+
+public class ArrayGugudan {
+    public static void main(String[] args) {
+        // 크기가 9인 배열을 생성한다.
+        int[] results = new int[9];
+
+        // results 배열의 크기만큼 반복한다.
+        for (int i = 0; i < results.length; i++) {
+            // 0 ~ 8까지 배열에 값을 초기화한다.
+            results[i] = 2 * (i + 1);
+        }
+
+        // results 배열의 크기만큼 반복한다.
+        for (int i = 0; i < results.length; i++) {
+            // 0 ~ 8까지 배열의 값을 출력한다.
+            System.out.println("2 * " + (i + 1) + " = " + results[i]);
+        }
+
+        // results 변수에 새로운 배열을 생성한다.
+        results = new int[9];
+
+        for (int i = 2; i < 10; i++) {
+            System.out.println(i + "단");
+            for (int j = 0; j < results.length; j++) {
+                results[j] = i * (j + 1);
+                System.out.println(i + " * " + (j + 1) + " = " + results[j]);
+            }
+        }
+    }
+}
--- src/inflearn/Gugudan.java
+++ src/inflearn/Gugudan.java
@@ -1,18 +1,71 @@
 package inflearn;
 
+import java.util.Scanner;
+
 public class Gugudan {
     public static void main(String[] args) {
-        //2단
+        /*
+        // 2단
         // 0 ~ 9까지 반복한다.
         for(int i = 1; i < 10; i++) {
-            // 2의 배수 10개의 값을 출력한다.
+            // 2의 배수 9개의 값을 출력한다.
             System.out.println("2 * " + i + " = " + 2 * i);
         }
-        //3단
+        // 3단
         // 0 ~ 9까지 반복한다.
         for(int i = 1; i < 10; i++) {
-            // 3의 배수 10개의 값을 출력한다.
+            // 3의 배수 9개의 값을 출력한다.
             System.out.println("3 * " + i + " = " + 3 * i);
         }
+
+        // 4단 ~ 5단
+        // 입력값 받기
+        System.out.println("구구단 중 출력할 단은? : ");
+        Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
+
+        System.out.println(number + " * 1 = " + number * 1);
+        System.out.println(number + " * 2 = " + number * 2);
+        System.out.println(number + " * 3 = " + number * 3);
+        System.out.println(number + " * 4 = " + number * 4);
+        System.out.println(number + " * 5 = " + number * 5);
+        System.out.println(number + " * 6 = " + number * 6);
+        System.out.println(number + " * 7 = " + number * 7);
+        System.out.println(number + " * 8 = " + number * 8);
+        System.out.println(number + " * 9 = " + number * 9);
+
+
+        // 6단
+        // 변수 i를 1로 초기화 한다.
+        int i = 1;
+        // i가 10 미만 일경우 반복문을 종료한다.
+        while (i < 10) {
+            // 6의 배수 9개의 값을 출력한다.
+            System.out.println("6 * " + i + " = " + 6 * i);
+            // i의 값이 1 증가한다.
+            i++;
+        }
+
+        // 7단
+        for (int j = 1; j < 10; j++) {
+            // 7의 배수 9개의 값을 출력한다.
+            System.out.println("7 * " + j + " = " + 7 * j);
+        }
+        */
+
+        // 8단 ~ 9단
+        // 입력값 받기
+        System.out.println("구구단 중 출력할 단은? : ");
+        Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
+        System.out.println(number + "단");
+        if (number >= 2 && number <= 9) {
+            for (int i = 1; i < 10; i++) {
+                System.out.println(number + " * " + i + " = " + number * i);
+            }
+        } else {
+            System.out.println("2이상, 9이하의 값만 입력할 수 있습니다.");
+        }
+
     }
 }
