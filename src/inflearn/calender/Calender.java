6a80e13952768c7e46e15cdb32f1c4bfa367fdc7
seominah <seominah95@gmail.com>
2021-12-21 21:39:46 +0900

    Implement get last day of month using method

---
+++ src/inflearn/calender/Calender.java
@@ -0,0 +1,45 @@
+package inflearn.calender;
+
+import java.util.Scanner;
+
+public class Calender {
+    public static int maxDayOfMonth (int month) {
+        int[] months = new int[12];
+
+        for (int i = 0; i < 7; i++) {
+            if (i == 1) {
+                months[i] = 28;
+            } else if (i % 2 == 0) {
+                months[i] = 31;
+            } else {
+                months[i] = 30;
+            }
+        }
+
+        for (int i = 7; i < 12; i++) {
+            if (i % 2 == 0) {
+                months[i] = 30;
+            } else {
+                months[i] = 31;
+            }
+        }
+
+        return months[month - 1];
+    }
+
+    public static void main(String[] args) {
+
+        // 숫자를 입력 받아 해당하는 달의 최대 일수를 출력하는 프로그램
+        // 31일 1, 3, 5, 7, 8, 10, 12
+        // 30일 4, 6, 9, 11
+        // 28일 2
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("달을 입력하세요.");
+        int month = scanner.nextInt();
+
+        System.out.printf("%d월은 %d일까지 있습니다.", month, maxDayOfMonth(month));
+        scanner.close();
+    }
+}
