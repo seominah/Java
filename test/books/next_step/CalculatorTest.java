1463dfc39ba483d1ddf2a36e46134ee704853dc7
seominah <seominah95@gmail.com>
2022-02-11 17:16:15 +0900

    Updated Using assertEquals Method

--- test/books/next_step/CalculatorTest.java
+++ test/books/next_step/CalculatorTest.java
@@ -5,28 +5,25 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class CalculatorTest {
+    private Calculator cal = new Calculator();
 
     @Test
     void add() {
-        Calculator cal = new Calculator();
         assertEquals(5, cal.add(2, 3));
     }
 
     @Test
     void subtract() {
-        Calculator cal = new Calculator();
         assertEquals(3, cal.subtract(6,3));
     }
 
     @Test
     void multiply() {
-        Calculator cal = new Calculator();
         assertEquals(10, cal.multiply(2, 5));
     }
 
     @Test
     void divide() {
-        Calculator cal = new Calculator();
         assertEquals(4, cal.divide(12, 3));
     }
 }
