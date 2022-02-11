3c55e7d367bb0e4cd074e8b0fae3fd750f658f8d
seominah <seominah95@gmail.com>
2022-02-11 16:32:09 +0900

    Separated Using Method within Test Code

--- src/books/next_step/CalculatorTest.java
+++ src/books/next_step/CalculatorTest.java
@@ -3,9 +3,27 @@ package books.next_step;
 public class CalculatorTest {
     public static void main(String[] args) {
         Calculator cal = new Calculator();
-        System.out.println(cal.add(5, 3));
-        System.out.println(cal.subtract(7, 2));
-        System.out.println(cal.multiply(2, 5));
+        add(cal);
+        subtract(cal);
+        multiply(cal);
+        divide(cal);
+    }
+
+    private static void divide(Calculator cal) {
         System.out.println(cal.divide(12, 3));
+
+    }
+
+    private static void multiply(Calculator cal) {
+        System.out.println(cal.multiply(2, 5));
+    }
+
+    private static void subtract(Calculator cal) {
+        System.out.println(cal.subtract(7, 2));
+    }
+
+    private static void add(Calculator cal) {
+        System.out.println(cal.add(5, 3));
+
     }
 }
